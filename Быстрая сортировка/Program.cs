9159022ddaad1fb966cb59ec93fb605b75ccbb29
«Vadim <vvaganoff@mail.ru>
2023-01-17 12:58:46 +0400

    Добавлен пример  быстрой сортировки

--- Быстрая сортировка/Program.cs
+++ Быстрая сортировка/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+int[] res = QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив: [{string.Join(", ", res)}]");
+int[] QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if(minIndex >= maxIndex) return inputArray;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return inputArray;
+}
+
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for(int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(ref inputArray[pivotIndex], ref inputArray[i]);
+        }
+    }
+    pivotIndex++;
+    Swap(ref inputArray[pivotIndex], ref inputArray[maxIndex]);
+    return pivotIndex;
+}
+
+void Swap(ref int leftValue, ref int rightValue)
+{
+    int temp = leftValue;
+    leftValue = rightValue;
+    rightValue = temp;
+}
